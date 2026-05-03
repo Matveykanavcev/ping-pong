3e46deb78badc8a6fd2ce1316ba3717f78efa5f9
imprezza <154687960+Matveykanavcev@users.noreply.github.com>
2026-05-03 18:51:51 +0300

    Add files via upload

--- game.py
+++ game.py
@@ -13,6 +13,7 @@ FPS = 60
 
 font.init()
 lose_font = font.SysFont("Arial", 50)
+score_font = font.SysFont("Arial", 30)
 
 
 class Racket:
@@ -48,11 +49,21 @@ class Ball:
         self.rect.y += self.speed_y
 
     def bounce(self, left_racket, right_racket):
+        hit_left = False
+        hit_right = False
+
         if self.rect.top <= 0 or self.rect.bottom >= HEIGHT:
             self.speed_y *= -1
 
-        if self.rect.colliderect(left_racket.rect) or self.rect.colliderect(right_racket.rect):
+        if self.rect.colliderect(left_racket.rect):
             self.speed_x *= -1
+            hit_left = True
+
+        if self.rect.colliderect(right_racket.rect):
+            self.speed_x *= -1
+            hit_right = True
+
+        return hit_left, hit_right
 
     def draw(self):
         draw.circle(
@@ -66,6 +77,9 @@ class Ball:
 left_racket = Racket(10, 200, 20, 100, 10)
 right_racket = Racket(670, 200, 20, 100, 10)
 ball = Ball(WIDTH // 2, HEIGHT // 2, 25, 10, 10)
+
+left_score = 0
+right_score = 0
 
 game = True
 result_text = None
@@ -81,7 +95,12 @@ while game:
     right_racket.move_racket2(ball)
 
     ball.move()
-    ball.bounce(left_racket, right_racket)
+    hit_left, hit_right = ball.bounce(left_racket, right_racket)
+
+    if hit_left:
+        left_score += 1
+    if hit_right:
+        right_score += 1
 
     if ball.rect.left <= 0:
         result_text = lose_font.render("LOSE", True, (255, 0, 0))
@@ -97,6 +116,9 @@ while game:
     right_racket.draw((2, 207, 230))
     ball.draw()
 
+    score_text = score_font.render(f"{left_score} : {right_score}", True, (255, 255, 255))
+    window.blit(score_text, (WIDTH // 2 - 40, 20))
+
     display.update()
     clock.tick(FPS)
 
@@ -104,7 +126,7 @@ while game:
 window.blit(background, (0, 0))
 
 if result_text:
-    window.blit(result_text, (180, 220))
+    window.blit(result_text, (285, 200))
 
 display.update()
 time.delay(3000)
